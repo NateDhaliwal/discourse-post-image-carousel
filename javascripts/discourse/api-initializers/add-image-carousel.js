668461038cfbff9ef35705a5733fc129138fae85
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-10-24 16:17:12 +0800

    Use empty placeholder

--- javascripts/discourse/api-initializers/add-image-carousel.js
+++ javascripts/discourse/api-initializers/add-image-carousel.js
@@ -3,9 +3,9 @@ import { apiInitializer } from "discourse/lib/api";
 export default apiInitializer((api) => {
   api.addComposerToolbarPopupMenuOption({
     action: (toolbarEvent) => {
-      toolbarEvent.applySurround(`[carousel=${settings.carousel_software}]\n[image]`, "[/image]\n[/carousel]");
+      toolbarEvent.applySurround(`[carousel=${settings.carousel_software}]\n[image]`, "[/image]\n[/carousel]", "");
     },
-    icon: 'fas-images',
+    icon: 'far-images',
     label: 'add_image_carousel'
   });
 });
