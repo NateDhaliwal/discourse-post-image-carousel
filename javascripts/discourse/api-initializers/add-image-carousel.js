eaef3065e0d3b7d97d1b8d8679735b07151e8058
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-10-24 16:10:09 +0800

    Change far-images to fa-images

--- javascripts/discourse/api-initializers/add-image-carousel.js
+++ javascripts/discourse/api-initializers/add-image-carousel.js
@@ -3,9 +3,9 @@ import { apiInitializer } from "discourse/lib/api";
 export default apiInitializer((api) => {
   api.addComposerToolbarPopupMenuOption({
     action: (toolbarEvent) => {
-      toolbarEvent.applySurround(`[carousel=${settings.carousel_software}]\n[image][/image]`, "[/wrap]");
+      toolbarEvent.applySurround(`[carousel=${settings.carousel_software}]\n[image]`, "[/image]\n[/carousel]");
     },
-    icon: 'far-images',
+    icon: 'fa-images',
     label: 'add_image_carousel'
   });
 });
