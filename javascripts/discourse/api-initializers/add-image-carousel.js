3e04496a94f34e9df72dd39ebe5a868227a17e7e
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-10-24 18:09:28 +0800

    Use onToolbarCreate instead

--- javascripts/discourse/api-initializers/add-image-carousel.js
+++ javascripts/discourse/api-initializers/add-image-carousel.js
@@ -3,20 +3,21 @@ import I18n from "discourse-i18n";
 
 export default apiInitializer((api) => {
   const currentLocale = I18n.currentLocale();
-  I18n.translations[currentLocale].js.composer.image_carousel_placeholder =
-    settings.image_carousel_placeholder;
+  I18n.translations[currentLocale].js.composer.image_carousel_placeholder = settings.image_carousel_placeholder;
 
-  api.addComposerToolbarPopupMenuOption({
-    id: "image-carousel",
-    group: "extras",
-    perform: (toolbarEvent) => {
-      toolbarEvent.applySurround(
-        `[carousel=${settings.carousel_software}]\n[image]`,
-        "[/image]\n[/carousel]",
-        "image_carousel_placeholder"
-      );
-    },
-    icon: "images",
-    title: themePrefix("add_image_carousel")
+  api.onToolbarCreate((toolbar) => {
+    toolbar.addButton({
+      id: "image-carousel",
+      group: "extras",
+      icon: "images",
+      title: themePrefix("add_image_carousel"),
+      perform: (e) => {
+        e.applySurround(
+          `[carousel=${settings.carousel_software}]\n[image]`,
+          "[/image]\n[/carousel]",
+          "image_carousel_placeholder"
+        );
+      }
+    });
   });
 });
