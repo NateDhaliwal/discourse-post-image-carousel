00e4c1f734a5d77972bcea60a76c6ebdf96a4fca
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-10-24 16:21:36 +0800

    Add locales

--- javascripts/discourse/api-initializers/add-image-carousel.js
+++ javascripts/discourse/api-initializers/add-image-carousel.js
@@ -1,11 +1,22 @@
 import { apiInitializer } from "discourse/lib/api";
+import I18n from "discourse-i18n";
 
 export default apiInitializer((api) => {
+  const currentLocale = I18n.currentLocale();
+  I18n.translations[currentLocale].js.composer.image_carousel_placeholder =
+    settings.image_carousel_placeholder;
+
   api.addComposerToolbarPopupMenuOption({
-    action: (toolbarEvent) => {
-      toolbarEvent.applySurround(`[carousel=${settings.carousel_software}]\n[image]`, "[/image]\n[/carousel]", "");
+    id: "image-carousel",
+    group: "extras",
+    perform: (toolbarEvent) => {
+      toolbarEvent.applySurround(
+        `[carousel=${settings.carousel_software}]\n[image]`,
+        "[/image]\n[/carousel]",
+        "image_carousel_placeholder"
+      );
     },
-    icon: 'far-images',
-    label: 'add_image_carousel'
+    icon: "images",
+    title: themePrefix("add_image_carousel")
   });
 });
